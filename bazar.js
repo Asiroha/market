e64323dca8eb49e9c13084272c93b917de34c7bd
Asiroha <niksiroha@gmail.com>
2025-07-27 11:27:53 +0300

    Опис змін

--- bazar.js
+++ bazar.js
@@ -5,7 +5,78 @@ class VidgukObject {
         this.vidguk = vidguk
     }
 }
+let administrator = {
+    name: "Власник Базару",
+    ava: "https://tse1.mm.bing.net/th/id/OIP.r74Xykv0CJXTQbXJZa8NEwHaLG?w=1708&h=2560&rs=1&pid=ImgDetMain&o=7&rm=3",
+
+}
+administratorbot = {
+    name: "БОТ",
+    ava: "profil.png",
+
+}
+let usersAll = []
+let users = []
+let thisuser
+function sms(avtor, text) {
+    let avtorstatus = (avtor === administrator && avtor === administratorbot ) ? "АДМІНІСТРАЦІЯ" : "користувач";
+
+
+    let smstext = perehid(text, 3);
+
+    let smshtml = document.createElement('div');
+    smshtml.className = 'sms';
+    smshtml.innerHTML = `
+      <div class="usrow">
+          <img class="avatar2" src="${avtor.ava}">
+          <p class="username1">${avtor.name}</p>
+          <p class="avtorstatus">${avtorstatus}</p>
+      </div>
+      <p class='textsms'>${smstext}</p>
+    `;
+
+    let pole = document.querySelector('.smspole');
+    pole.appendChild(smshtml);
+
+    
+    let allsms = pole.querySelectorAll('.sms');
+
+    
+    allsms.forEach((el, i) => {
+        let indexFromLast = allsms.length - 1 - i;
+
+
+        if (indexFromLast >= 1) {
+            el.style.opacity = '0.7';
+            el.style.transform = 'scale(0.85)';
+        }
+        if (indexFromLast >= 2) {
+            el.style.opacity = '0.5';
+            el.style.transform = 'scale(0.7)';
+        }
+        if (indexFromLast >= 3) {
+            el.style.opacity = '0.3';
+            el.style.transform = 'scale(0.5)';
+        }
+        if (indexFromLast >= 4) {
+            el.remove(); 
+        }
+    });
+
+    setTimeout(() => {
+        smshtml.style.animation = 'opacityend 4s';
+    }, 4500);
+
+    setTimeout(() => {
+        smshtml.remove();
+
+    }, 8400);
+}
+
+sms(administrator ,'ви зарегалились вітаєм оащпо оавзшлп вапощап')
+sms(administrator ,'ви зарегалились вітаєм')
 let tovars = []
+
 function saveTovarsToLocalStorage() {
   const simpleTovars = tovars.map(t => ({
     nametovar: t.nametovarsearch,
@@ -36,11 +107,29 @@ function loadTovarsFromLocalStorage() {
 }
 let storinka = 1
 let numreiting = 0
+let regbg = ['regbg.png', 'https://cdn.hudy.cz/images/w1920h1200-cover-2x/2/33652.jpg',
+'https://live.staticflickr.com/65535/52535942118_92829ac578_b.jpg',
+'https://i.etsystatic.com/27744849/r/il/08b82b/2916257755/il_fullxfull.2916257755_e7lc.jpg',
+'https://wallpaper.forfun.com/fetch/34/3484413e87569d10be4dbbec88681ed4.jpeg'
+]
+let images = [];
+regbg.forEach(src => {
+let img = new Image();
+img.src = src;
+images.push(img);
+});
+
+let index = 0;
+let registerPole = document.querySelector('.registerpole');
 function displaystorynky(){
 if(storinka === 1){
      document.querySelector('.place1').style.display = "flex"
     for (let tovar of alltovars) {
         tovar.display();
+
+    }
+    for (let tovar of alltovars) {
+        tovar.user.tovars.push(tovar);
 
     }
 }
@@ -56,10 +145,6 @@ else{
 }
 if(storinka === 3){
     document.querySelector('.profil').style.display = "flex"
-        for (let tovar of alltovars) {
-        tovar.user.tovars.push(tovar);
-
-    }
 }
 else{
     document.querySelector('.profil').style.display = "none"
@@ -84,6 +169,25 @@ if(storinka === 6){
 }
 else{
     document.querySelector('.myprofil').style.display = "none"
+}
+if(storinka === 7){
+    document.querySelector('.registerpole').style.display = "flex"
+
+
+    setInterval(() => {
+    registerPole.style.backgroundImage = `url(${regbg[index]})`;
+    index = (index + 1) % regbg.length;
+    }, 2100);
+}
+else{
+    document.querySelector('.registerpole').style.display = "none"
+}
+if(storinka === 8){
+    document.querySelector('.myAccaunt').style.display = "flex"
+
+}
+else{
+    document.querySelector('.myAccaunt').style.display = "none"
 }
 }
 
@@ -140,7 +244,9 @@ function algorytm2(n1, n2, ng) {
  
 class User{
     constructor(name, ava, info, number, stash,
-         prodash, reiting1, dialog){
+         prodash, reiting1, dialog, pasword, usAcc = 0){
+        this.usAcc = usAcc
+        this.pasword = pasword
         this.vidguky = []
         this.name = name
         this.ava = ava
@@ -361,9 +467,9 @@ this.element.querySelector('.ocinkytovaru3').style.width = `${algorytm2(this.rei
         
         }
     }
-    chektovar(){
+    chektovar(element){
         for (let tovar of this.tovars) {
-            this.element.querySelector('.profiltovars').appendChild(tovar.element);
+            element.querySelector('.profiltovars').appendChild(tovar.element);
             }
     }
 }
@@ -398,25 +504,27 @@ class Tovar {
         document.querySelector('.tovarinfoall').innerHTML = `
             <div class="thistovar">
                 <div class="usrow">
-                    <img class='avatar2' src=${this.user.ava}>
+                    <img class="avatar2" src="${this.user.ava}">
                     <p class="username1">${this.user.name}</p>
                 </div>
-                <img class="img3" src=${this.img1}>
+                <img class="img3" src="${this.img1}">
                 <div class="poslygitovaru">
                     <p class="pokupkabut">купити</p>
-                    <p class="korzinabut">у корзину <img class="img1" src='korzina.png'></p>
-                    <p class="chatuserbut">чат з продавцем<img class="img1" src="sms.png"></p>
+                    <p class="korzinabut">у корзину <img class="img1" src="korzina.png"></p>
+                    <p class="chatuserbut">чат з продавцем <img class="img1" src="sms.png"></p>
                     <p class="nazad">назад</p>
                 </div>
             </div>
+
             <div class="thistovar">
                 <p>${this.nametovar}</p>
-                <p>${this.pricetovar}грн</p>
+                <p>${this.pricetovar} грн</p>
                 <div class="opystovaru">
                     <h1>опис товару</h1>
                     <p>${this.infotovar}</p>
                 </div>
             </div>
+
         `;
 
         
@@ -428,7 +536,7 @@ class Tovar {
             storinka = 3;
             displaystorynky();
             this.user.display()
-            this.user.chektovar()
+            this.user.chektovar(this.user.element)
         });
         
         document.querySelector('.img3').addEventListener('click', () => {
@@ -454,38 +562,38 @@ class Tovar {
             }
         }
 }
+
 let alltovars = []
 let s1 = [2, 4.3, 5, 4, 4, 3.7, 2.1];
 let pomidorsp = ["https://auivf.com/wp-content/uploads/2019/05/tomatoes.jpg", "https://varus.ua/img/product/1140/1140/2561735"];
-let pomidorka = new User("помідорка", pomidorsp[0], "info", 0, 23, 23, [2, 4.5, 2, 5, 5, 5, 5, 2.7, 4, 4, 2], 456);
-let bubu = new User("бубу", "https://ih1.redbubble.net/image.3460018834.0538/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg", "бубубубубубубуббві абаабабаа оооооооо", 0, 23, 23, s1, 98);
-let bubu2 = new User("бубу", "https://ih1.redbubble.net/image.3460018834.0538/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg", "бубубубубубубуббві абаабабаа оооооооо", 0, 12, 20, [1, 2, 3, 4, 5,], 98);
-let users = [bubu2]
-let pomidor3 = new Tovar(pomidorsp, "Помідори3", 20, "Опис товару", bubu2);
-let usersAll = [bubu, pomidorka]
-let pomi = new Tovar(pomidorsp, "Помідори смачні", 50, "Опис товару", bubu);
-    let pomidor = new Tovar(pomidorsp, "Помідори смачні", 50, "Опис товару", pomidorka);
-    let pomidor2 = new Tovar(pomidorsp, "Помідори", 20, "Опис товару", pomidorka);
-    alltovars.push(pomi, pomidor, pomidor2);
-    bubu.tovars.push(pomi);
-    pomidorka.tovars.push(pomidor, pomidor2);
-    bubu.vidguky = [new VidgukObject(pomidorka, 2, 'в мене краще і вопще ти в мене вкрав шахрай'),
-        new VidgukObject(pomidorka, 2, 'ставте йому погані відгуки та оцінки'),
-        new VidgukObject(bubu, 1, 'ееееееееее оооооооо щорщощощоопдлддвп'),
-        new VidgukObject(bubu, 2, 'як відгуки видаляти еое ееее оооооо'),
-        new VidgukObject(bubu2, 2, 'як відгуки видаляти'),
-    ]
-    
-    bubu2.vidguky = [new VidgukObject(pomidorka, 2, 'в мене краще і вопще ти в мене вкрав шахрай')]
-loadTovarsFromLocalStorage()
-saveTovarsToLocalStorage()
+let boba = new User('бубу', 'https://ih1.redbubble.net/image.3460018834.0538/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+    'бубубубубубубуббві абаабабаа оооооооо', 
+    1, 23,
+    4234, [2, 4.3, 5, 4, 4, 3.7, 2.1, 4, 4, 4,5, 4.9, 2.1, 3.3], 99, '12345')
+let pomidorka = new User('ПОМІДОР', 'https://auivf.com/wp-content/uploads/2019/05/tomatoes.jpg',
+    'помідорка купуйте помідори', 
+    1, 23,
+    4234, [2, 4.3, 5, 4, 4, 3.7, 2.1, 4, 4, 4,5, 4.9, 2.1, 3.3], 99, '12345')
+let pomidor = new Tovar(pomidorsp,  'tomato', 50, 'info', pomidorka)
+let pomidor2 = new Tovar(pomidorsp,  'tomato2', 50, 'info', pomidorka)
+let pomidor3 = new Tovar(pomidorsp,  'tomato2', 50, 'info', boba)
+alltovars =  [pomidor, pomidor2, pomidor3]
+usersAll = [pomidorka, boba]
+loadUsersFromLocalStorage()
+saveUsersToLocalStorage()
 for(tovar of tovars){
     alltovars.push(tovar)
 }
 let loginfalse = 1
+for(user of usersAll){
+    if(user.usAcc === 1){
+        thisuser = user
+        loginfalse = 0
+    }
+}
 document.querySelector('.startprofil').addEventListener('click', () => {
     storinka = 6
-    if(loginfalse){
+    if(loginfalse === 1){
     document.querySelector('.myprofil').innerHTML = `
     <h1>Нема акаунту</h1>
     <h2>зайдіть на свій акаунт</h2>
@@ -499,10 +607,144 @@ document.querySelector('.startprofil').addEventListener('click', () => {
         <p class="nazad">до головної сторінки</p>
     </div> 
     
+    `}
+    else{
+    storinka = 8
+    document.querySelector('.myAccaunt').innerHTML = `
+    <h1>ваш акаунт<h1>
+    <div class='usrow'>
+            <img class="img4" src=${thisuser.ava}>
+            <h1 class="nameprofil">${thisuser.name}</h1>
+    </div>
+    <div class="registinput">
+        <input class="namezmina" placeholder="(нове ім'я)" type="text">
+        <input class="namezmina" placeholder="(зображення профілю(посилання))" type="text">
+        <input class="namezmina" placeholder="(нове описання)" type="text">
+        <button class="createacc">змінити профіль</button>
+    </div> 
+    <h1>ваша статистика</h1>
+    <div class='statistyk'>
+        
+    </div>
+    <div class="poslygitovaru">
+        <p class="login">Увійти</p>
+        <p class="registr">зарегестрируватися</p>
+    </div>
+    <div class="poslygitovaru">
+        <p class="nazad">до головної сторінки</p>
+    </div> 
     `
+    }
+    document.querySelector('.login').addEventListener('click', () => {
+        storinka = 7
+        document.querySelector('.logininput').style.display = 'flex'
+        document.querySelector('.registinput').style.display = 'none'
+    document.querySelector('.loginacc').addEventListener('click', () => {
+            if(document.querySelector('.namelogin').value){
+                document.querySelector('.namelogin').style.border = "5px solid green"
+            }
+            else{
+                document.querySelector('.namelogin').style.border = "5px solid red"
+            }
+            if(document.querySelector('.pasword').value){
+                document.querySelector('.pasword').style.border = "5px solid green"
+            }
+            else{
+                document.querySelector('.pasword').style.border = "5px solid red"
+            }
+    if(document.querySelector('.pasword').value && document.querySelector('.namelogin').value){
+        let found = false;
+        for(usr of usersAll){
+            if(usr.name === document.querySelector('.namelogin').value){
+                found = true;
+                if(usr.pasword === document.querySelector('.pasword').value){
+                    thisuser = usr;
+                    storinka = 6
+                    usr.usAcc = 1
+                    saveUsersToLocalStorage()
+
+                    displaystorynky();
+                } else {
+                    sms(administratorbot, 'пароль неправильний');
+                }
+            }
+        }
+        if (!found) {
+            sms(administratorbot, 'користувача з таким ім`ям не існує');
+        }
+    }
+
+         })
     displaystorynky()
-}
+    })
+    document.querySelector('.registr').addEventListener('click', () => {
+        storinka = 7
+        document.querySelector('.registinput').style.display = 'flex'
+        document.querySelector('.logininput').style.display = 'none'
+            document.querySelector('.createacc').addEventListener('click', () => {
+            let numberegistr = 0
+            
+            
+            let number = document.querySelector('.numberegistr').value.split("").filter(c => /\d/.test(c))
+            
+            let nameregistr = 0 
+            const validName = /^[A-Za-zА-Яа-яґҐєЄіІїЇ0-9\s]{6,18}$/.test(document.querySelector('.nameregistr').value);
+
+            let paswordregistr = 0 
+            const paswordreg = /^[A-Za-zА-Яа-яґҐєЄіІїЇ0-9\s]{6,30}$/.test(document.querySelector('.paswordregistr').value)
+            if (number.length >= 9 && number.length <= 13) {
+                    numberegistr =  document.querySelector('.numberegistr').value;
+                    document.querySelector('.numberegistr').style.border = "5px solid green";
+            } 
+            else {
+                    document.querySelector('.numberegistr').style.border = "5px solid red";
+            }
+
+            if (validName) {
+                    nameregistr = document.querySelector('.nameregistr').value;
+                    document.querySelector('.nameregistr').style.border = "5px solid green";
+            } 
+            else {
+                    document.querySelector('.nameregistr').style.border = "5px solid red";
+            }
+            if (paswordreg) {
+                    paswordregistr = document.querySelector('.paswordregistr').value;
+                    document.querySelector('.paswordregistr').style.border = "5px solid green";
+            } 
+            else {
+                    document.querySelector('.paswordregistr').style.border = "5px solid red";
+            }
+            if(paswordregistr && nameregistr && numberegistr){
+                let stopadd1 = 1
+                for(u of usersAll){
+                    if(u.name === nameregistr){
+                        sms(administratorbot, `Упс нажаль користувач з ім'ям ${nameregistr} вже є придумай щось нове`)
+                        stopadd1 = 0
+                        document.querySelector('.registinput').style.border = "5px solid red"
+                    }
+                }
+                if(stopadd1 === 1){
+                let imuser = new User(nameregistr, 'profil.png',
+                    'Додайте інформацію', 
+                    numberegistr, 'недавно',
+                    0, [], 99, paswordregistr )
+                usersAll.push(imuser)
+                users.push(imuser)
+                saveUsersToLocalStorage()
+                document.querySelector('.registinput').style.border = "5px solid green"
+                }
+            }
+
+    
+    
+    })
+        displaystorynky()
+    })
+    displaystorynky()
+
 })
+
+
 let animka1 = 0;
 const searchbut = document.querySelector('.searchbut');
 searchbut.addEventListener('click', () => {
@@ -526,5 +768,58 @@ searchbut.addEventListener('click', () => {
         });
     }, 1000);
 });
+function saveUsersToLocalStorage() {
+    const simpleUsers = users.map(u => ({
+        name: u.name,
+        ava: u.ava,
+        info: u.info,
+        number: u.number,
+        stash: u.stash,
+        prodash: u.prodash,
+        reiting1: u.reitingS,
+        dialog: u.dialog,
+        pasword: u.pasword,
+        usAcc:  u.usAcc,
+        vidguky: u.vidguky.map(v => ({
+            userName: v.user.name,
+            vidgukR: v.vidgukR,
+            vidguk: v.vidguk
+        }))
+    }));
+    localStorage.setItem('users', JSON.stringify(simpleUsers));
+}
+
+function loadUsersFromLocalStorage() {
+    const usersData = JSON.parse(localStorage.getItem('users')) || [];
+    users = [];
+
+    for (let item of usersData) {
+        const user = new User(
+            item.name,
+            item.ava,
+            item.info.replace(/<br>/g, ' '),
+            item.number,
+            item.stash,
+            item.prodash,
+            item.reiting1,
+            item.dialog,
+            item.pasword,
+            item.usAcc,
+        );
+        users.push(user);
+        usersAll.push(user)
+        console.log(user)
+    }
+
+    for (let i = 0; i < usersData.length; i++) {
+        const item = usersData[i];
+        const user = users[i];
+
+        user.vidguky = item.vidguky.map(v => {
+            const foundUser = users.find(u => u.name === v.userName);
+            return new VidgukObject(foundUser || { name: v.userName, ava: "" }, v.vidgukR, v.vidguk);
+        });
+    }
+}
 
 displaystorynky();
